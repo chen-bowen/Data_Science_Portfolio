9a8129ebe8e8cceae461f09ea1c4c60662391e80
Bowen Chen <chenbowen184@gmail.com>
2018-08-11 18:55:00 -0700

    Update train.py

--- Project 2 - Image Classifier Application/Application/train.py
+++ Project 2 - Image Classifier Application/Application/train.py
@@ -8,7 +8,7 @@ def main():
                           'hidden_units' : args.hidden_units, 'learning_rate': args.learning_rate, 
                          'dropout_prob': args.dropout_prob}
         
-     model = train_model(hyperparameters, data_dir= 'flowers', device = args.gpu)
+     train_model(hyperparameters, data_dir= args.data_directory, save_dir = args.save_dir, device = args.gpu)
      return model
 if __name__ == '__main__':
     model = main()
