8a6d67ce01800b772c017dccbd1d9aed74180fdd
Bowen Chen <chenbowen184@gmail.com>
2018-08-12 09:01:26 -0700

    Update train.py

--- Project 2 - Image Classifier Application/Application/train.py
+++ Project 2 - Image Classifier Application/Application/train.py
@@ -2,13 +2,13 @@ from model_spec import *
 import argparse
 
 def main():
-     args = get_input_args_train()
+     args = get_input_args()
      # get hyperparameters
-     hyperparameters =  {'architecture': args.arch, 'epochs': args.epochs, 'print_every': args.print_every, 
+     hyperparameters =  {'architecture': args.arch, 'epochs': args.epoch, 'print_every': args.print_every, 
                           'hidden_units' : args.hidden_units, 'learning_rate': args.learning_rate, 
                          'dropout_prob': args.dropout_prob}
         
-     model = train_model(hyperparameters, data_dir= 'flowers', device = args.gpu)
-     return model
+     train_model(hyperparameters, data_dir= args.data_directory, save_dir = args.save_dir, device = args.gpu)
+    
 if __name__ == '__main__':
-    model = main()
+    main()
